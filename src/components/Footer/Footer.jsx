1d4aaf58aab91f0ffe98d5a2de3a98f591fd38d6
Binazir <binazir@MacBook-Air-Binazir.local>
2022-03-21 18:17:59 +0600

    react

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+const Footer = (props) => {
+  const arr = [1, 2, 3, 4, 5];
+  props.getArr(arr);
+  return (
+    <div>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#home">Footer </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Binazir Usenkanova</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Footer;
